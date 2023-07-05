f9138523a74e3b2013f0a8b1c5faee4f5dfdd722
unknown <sanjeevdandin@gmail.com>
2023-07-05 22:10:40 +0530

    learning standard input /output functionality basics

--- goLearn/goStdInputOutput/goStdInputOutput.go
+++ goLearn/goStdInputOutput/goStdInputOutput.go
@@ -2,6 +2,32 @@ package goStdIO
 
 import "fmt"
 
+var first string
+var second string
+
 func Gostdinputoutput() {
-	fmt.Println("input")
+	enterUserInput()
+	printOutput()
+}
+
+func enterUserInput() {
+	// Println function is used to
+	// display output in the next line
+	fmt.Println("Enter Your First Name: ")
+
+	// Taking input from user
+	fmt.Scanln(&first)
+
+	fmt.Println("Enter Second Last Name: ")
+
+	fmt.Scanln(&second)
+}
+
+func printOutput() {
+	// Print function is used to
+	// display output in the same line
+	fmt.Print("Your Full Name is: ")
+
+	// Addition of two string
+	fmt.Print(first + " " + second)
 }
