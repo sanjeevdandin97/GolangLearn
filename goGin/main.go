a9721aed90652469a1241f940a8cf903b36cea5e
unknown <sanjeevdandin@gmail.com>
2023-07-08 21:45:40 +0530

    adding routes to gin framework

--- goGin/main.go
+++ goGin/main.go
@@ -4,17 +4,36 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var router *gin.Engine
+
+var goRoutes = [2]string{
+	"example",
+	"hello",
+}
+
 func main() {
+	routerInit()
+	createRoutes()
+	runServer()
+}
 
-	// Creates a gin router with default middleware
-	router := gin.Default()
+// Creates a gin router with default middleware
+func routerInit() {
+	router = gin.Default()
+}
 
-	// A handler for GET request on /example
-	router.GET("/example", func(c *gin.Context) {
+func createRoutes() {
+	for i := 0; i < len(goRoutes); i++ {
+		var message string = goRoutes[i]
+		// A handler for GET request on routes[i]
+		router.GET("/"+goRoutes[i], func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": message,
+			}) // gin.H is a shortcut for map[string]interface{}
+		})
+	}
+}
 
-		c.JSON(200, gin.H{
-			"message": "example",
-		}) // gin.H is a shortcut for map[string]interface{}
-	})
+func runServer() {
 	router.Run() // listen and serve on port 8080
 }
