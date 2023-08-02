3c5c3a39b2e5b6e22477ba9a7d05246555393282
unknown <sanjeevdandin@gmail.com>
2023-08-03 00:20:00 +0530

    Learning about interfaces

---
+++ goIntermediate/go_interfaces/README.md
@@ -0,0 +1,7 @@
+# Go Interfaces
+
+Learning about go interfaces
+
+## Resources
+
+[Go Interface - programiz.com](https://www.programiz.com/golang/interface)
--- goIntermediate/go_interfaces/go_interfaces.go
+++ goIntermediate/go_interfaces/go_interfaces.go
@@ -3,5 +3,56 @@ package go_interfaces
 import "fmt"
 
 func INIT() {
-	fmt.Println(`Success!`)
+	go_interfaces()
+}
+
+// interface
+type Shape interface {
+	area() float32
+	getName() string
+}
+
+// struct to implement interface
+type Rectangle struct {
+	length, breadth float32
+	name            string
+}
+
+type Triangle struct {
+	base, height float32
+	name         string
+}
+
+// use struct to implement area() of interface
+func (r Rectangle) area() float32 {
+	return r.length * r.breadth
+}
+
+func (t Triangle) area() float32 {
+	return (t.base * t.height) / 2
+}
+
+func (r Rectangle) getName() string {
+	return r.name
+}
+
+func (t Triangle) getName() string {
+	return t.name
+}
+
+// access method of the interface
+func calculate(s Shape) {
+	fmt.Println("Area of", s.getName(), ":", s.area())
+}
+
+// main function
+func go_interfaces() {
+
+	// assigns value to struct members
+	rect := Rectangle{7, 4, "Rectangle"}
+	tri := Triangle{10, 5, "Triangle"}
+
+	// call calculate() with struct variable rect
+	calculate(rect)
+	calculate(tri)
 }
